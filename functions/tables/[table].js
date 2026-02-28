215f360ff01d5cc2721cd1d25de7aae834585627
jisoo3040 <psichoi95@gmail.com>
2026-02-28 14:11:41 +0900

    add pages functions for d1 api

---
+++ functions/tables/[table].js
@@ -0,0 +1,24 @@
+export async function onRequest(context) {
+  const table = context.params.table;
+  const { DB } = context.env;
+
+  if (context.request.method === 'GET') {
+    const { results } = await DB.prepare(`SELECT * FROM ${table}`).all();
+    return Response.json(results);
+  }
+
+  if (context.request.method === 'POST') {
+    const body = await context.request.json();
+    const keys = Object.keys(body);
+    const values = Object.values(body);
+
+    const placeholders = keys.map(() => '?').join(',');
+    await DB.prepare(
+      `INSERT INTO ${table} (${keys.join(',')}) VALUES (${placeholders})`
+    ).bind(...values).run();
+
+    return new Response('Inserted', { status: 201 });
+  }
+
+  return new Response('Method Not Allowed', { status: 405 });
+}
